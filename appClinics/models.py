69834875004e2577ff2a8086866b63e4c8a47487
QuangS2 <2051052108Quang@ou.edu.vn>
2022-11-30 20:04:36 +0700

    models done

--- appClinics/models.py
+++ appClinics/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Float, Boolean, Text, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from appClinics import db, app
 from enum import Enum as BaseEnum
+import json
 # from enum import Enum as GenderEnum
 # from enum import Enum as ActionEnum
 from flask_login import UserMixin
@@ -96,9 +97,10 @@ class MedicalReport(BaseModel):  # PhieuKham
 
 class Medicine(BaseModel):  # thuoc
     __tablename__ = 'Medicine'
-    name = Column(String(50), nullable=False)
-    content = Column(String(50), nullable=False)
-    unit = Column(String(20), nullable=False)
+    name = Column(String(75), nullable=False)
+    content = Column(String(255), nullable=False)
+    unit = Column(String(50), nullable=False)
+    price = Column(Integer,nullable=False)
     active = Column(Boolean, default=True)
 
     def __str__(self):
@@ -135,13 +137,13 @@ class ManageMedicine(BaseModel):  # quantriThuoc
     action = Column(Enum(ActionMedicineRole), nullable=False)
 
 
-class Regulations(BaseModel):  # quantriThuoc
+class Regulations(BaseModel):  # quyDinh
     __tablename__ = 'Regulations'
     patientAmount = Column(Integer, nullable=False)
     medicalExpenses = Column(Integer, nullable=False)
 
 
-class ManageRegulation(BaseModel):  # quantriThuoc
+class ManageRegulation(BaseModel):  # quanLyQuyDinh
     __tablename__ = 'ManageRegulation'
     action_id = Column(Integer, ForeignKey(Action.id), nullable=False)
     regulation_id = Column(Integer, ForeignKey(Regulations.id), nullable=False)
@@ -153,21 +155,7 @@ class ManageRegulation(BaseModel):  # quantriThuoc
 #                     Column('tag_id', ForeignKey('tag.id'), nullable=False, primary_key=True))
 #
 #
-# class Product(BaseModel):
-#     __tablename__ = 'product'
-#
-#     name = Column(String(50), nullable=False)
-#     description = Column(Text)
-#     price = Column(Float, default=0)
-#     image = Column(String(100))
-#     active = Column(Boolean, default=True)
-#     # category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
-#     tags = relationship('Tag', secondary='prod_tag', lazy='subquery',
-#                         backref=backref('products', lazy=True))
-#     receipt_details = relationship('ReceiptDetails', backref='product', lazy=True)
-#
-#     def __str__(self):
-#         return self.name
+
 #
 #
 # class Tag(BaseModel):
@@ -214,24 +202,16 @@ if __name__ == '__main__':
         # db.session.add(u)
         # db.session.commit()
 
-        # c1 = Category(name='Điện thoại')
-        # c2 = Category(name='Máy tính bảng')
-        # c3 = Category(name='Phụ kiện')
-        #
-        # db.session.add_all([c1, c2, c3])
-        # db.session.commit()
 
-        # p1 = Product(name='Galaxy S22 Pro', description='Samsung, 128GB', price=25000000,
-        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729569/fi9v6vdljyfmiltegh7k.jpg',
-        #              category_id=1)
-        # p2 = Product(name='Galaxy Fold 4', description='Samsung, 128GB', price=38000000,
-        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg',
-        #              category_id=1)
-        # p3 = Product(name='Apple Watch S5', description='Apple, 32GB', price=18000000,
-        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729569/fi9v6vdljyfmiltegh7k.jpg',
-        #              category_id=3)
-        # p4 = Product(name='Galaxy Tab S8', description='Samsung, 128GB', price=22000000,
-        #              image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729569/fi9v6vdljyfmiltegh7k.jpg',
-        #              category_id=2)
-        # db.session.add_all([p1, p2, p3, p4])
-        # db.session.commit()
+        # with open(f'data/medicine.json', encoding='utf-8') as f:
+        #     datas = json.load(f)['result']['items']
+        #     for item in datas:
+        #         if len(item['hoatChat']) < 255 and len(item['tenThuoc']) < 75 and len(item['donViTinh']) < 50:
+        #             m = Medicine(name = item['tenThuoc'],
+        #                          unit = item['donViTinh'],
+        #                          price = item['giaBanBuon'],
+        #                          content = item['hoatChat'])
+        #             db.session.add(m)
+        #             db.session.commit()
+
+
